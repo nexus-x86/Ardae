26e8dcc24484a73c9399b9a66d8726c2d5317b0c
nexus_x86 <60083031+nexus-x86@users.noreply.github.com>
2023-09-16 10:48:33 -0400

    basic template

--- main.py
+++ main.py
@@ -1,2 +1,53 @@
-from taipy import Gui
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
+db = SQLAlchemy(app)
+
+currentWord = ""
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(100), nullable=False)
+    guesses = db.relationship('Guess', backref='game', lazy=True)
+
+class Guess(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    song_name = db.Column(db.String(300), nullable=False)
+    is_correct = db.Column(db.Boolean, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/get-word', methods=['GET'])
+def get_word():
+    # put the dictionary api here, this is just dummy code for me
+    import random
+    words = ['love', 'world', 'life', 'night', 'music']
+    currentWord = random.choice(words)
+
+    game = Game(word=currentWord)
+    db.session.add(game)
+    db.session.commit()
+    return jsonify(word=currentWord, game_id=game.id)
+
+@app.route('/validate-song', methods=['POST'])
+def validate_song():
+    song_name = request.json.get('song_name')
+    game_id = request.json.get('game_id')
+    # dummy code for now, replace with actual music lyrics database/API check.
+    is_correct = currentWord in song_name
+    guess = Guess(song_name=song_name, is_correct=is_correct, game_id=game_id)
+    db.session.add(guess)
+    db.session.commit()
+    return jsonify(is_correct=is_correct)
+
+@app.route('/')
+def index():
+    return render_template('game.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ templates/game.js
@@ -0,0 +1,25 @@
+let word;
+let gameId;
+
+window.onload = async () => {
+    const response = await fetch('/get-word');
+    const data = await response.json();
+    word = data.word;
+    gameId = data.game_id;
+    document.getElementById('word').innerText = word;
+}
+
+async function validateSong() {
+    const songName = document.getElementById('songName').value;
+    const response = await fetch('/validate-song', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ song_name: songName, game_id: gameId })
+    });
+    const data = await response.json();
+    if(data.is_correct) {
+        alert("Correct!");
+    } else {
+        alert("Wrong!");
+    }
+}
